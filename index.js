8cdb15a5a416bd9ccce1aa39267ffa7c78969f7c
ARSparks30 <ar.sparks30@gmail.com>
2023-07-17 21:36:14 +0530

    Basic CSS code

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expresLayouts = require("express-ejs-layouts");
 
+app.use(expresLayouts);
 app.use("/", require("./routes"));
+app.use(express.static("./assets"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.set("layoutextractstyles", true);
+app.set("layoutextractscripts", true);
 // app.set("views", require("./views"));
 
 app.listen(port, function (err) {
